9605f0a51624f2359856633d2f7c506c33224975
Rishabh Kumar <rishabhbgt1@gmail.com>
2026-08-20 14:18:52 +0530

    Restore Gmail SMTP email service

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,33 +1,17 @@
-const { Resend } = require("resend");
-
-const resend = new Resend(
-    process.env.RESEND_API_KEY
-);
+const transporter = require("../config/mail");
 
 const sendEmail = async (to, subject, html) => {
     try {
-        const { data, error } =
-            await resend.emails.send({
-                from: "CampusOne <onboarding@resend.dev>",
-                to,
-                subject,
-                html,
-            });
+        await transporter.sendMail({
+            from: `"CampusOne Administration" <${process.env.SMTP_EMAIL}>`,
+            to,
+            subject,
+            html,
+        });
 
-        if (error) {
-            throw new Error(
-                error.message ||
-                "Email could not be sent"
-            );
-        }
-
-        return data;
+        return true;
     } catch (error) {
-        console.error(
-            "Email Send Error:",
-            error
-        );
-
+        console.error("Email Send Error:", error);
         throw error;
     }
 };
